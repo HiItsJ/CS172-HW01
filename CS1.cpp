8046f9cf83f08764159d4e3a41cd53157925c1c0
Josiah Gallegos <jgallegos20@my.whitworth.edu>
2016-09-13 15:25:48 -0700

    First Commit

---
+++ CS1.cpp
@@ -0,0 +1,18 @@
+//
+//  CS1.cpp
+//  
+//
+//  Created by Josiah on 9/13/16.
+//
+//
+
+#include "CS1.hpp"
+
+int main(){
+    void ex02(){
+        
+        bool hasPassedTest = true;
+        int 
+        
+    }
+}
---
+++ CS1.hpp
@@ -0,0 +1,14 @@
+//
+//  CS1.hpp
+//  
+//
+//  Created by Josiah on 9/13/16.
+//
+//
+
+#ifndef CS1_hpp
+#define CS1_hpp
+
+#include <stdio.h>
+
+#endif /* CS1_hpp */
