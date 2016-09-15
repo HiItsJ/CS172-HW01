03e315d7ddf728f690013096d903009dcea2d7a7
Josiah Gallegos <jgallegos20@my.whitworth.edu>
2016-09-15 15:38:53 -0700

    Continued assignment requirements.

--- CS1.cpp
+++ CS1.cpp
@@ -23,9 +23,23 @@ void ex02(){
     int y = rand() % 100;
     
     if (x > y){
-        cout<<"x is greater than y.";
+        cout<<"x is greater than y.\n";
     }
     if (x==y){
-        cout<<"x is equal to y.";
+        cout<<"x is equal to y.\n";
     }
+    
+    int numberOfShares;
+    cout<<"Enter a number: ";
+    cin>>numberOfShares;
+    if (numberOfShares < 100){
+        cout<<numberOfShares<<" is less than 100.\n";
+    }
+    
+    int input1;
+    int input2;
+    cout<<"Enter a box width: ";
+    cin>>input1;
+    cout<<"Enter a book width: ";
+    cin>>input2;
 }
